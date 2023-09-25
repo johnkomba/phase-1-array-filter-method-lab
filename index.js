a0cbcb45fa9b1341cd631d77e97809418f0af13c
Jkomba <johnkombah88@gmail.com>
2023-09-25 14:41:05 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+// Function to find drivers with matching names
+function findMatching(drivers, query) {
+    return drivers.filter(driver => driver.toLowerCase() === query.toLowerCase());
+  }
+  
+  // Function to find drivers whose names begin with provided letters
+  function fuzzyMatch(drivers, query) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(query.toLowerCase()));
+  }
+  
+  // Function to find drivers based on their name property
+  function matchName(drivers, query) {
+    return drivers.filter(driver => driver.name.toLowerCase() === query.toLowerCase());
+  }
+  
